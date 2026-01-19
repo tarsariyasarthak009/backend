2e02c40b1d49425aa182ee6019d90b8392370e5e
Sarthak Tarsariya <sarthakrajubhai@gmail.com>
2026-01-19 12:04:58 +0530

    user and video model create

---
+++ src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,
+        index : true
+    },
+    emial : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true
+    },
+    fullname : {
+        type : String,
+        required : true,
+        trim : true,
+        index : true
+    },
+    avatar : {
+        type : String, 
+        required : true,
+    },
+    coverImage : {
+        type : String,
+    },
+    watchHistory : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "video"
+        }
+    ],
+    password : {
+        type : String,
+        required : [true, "password is required"],
+    },
+    refreshToken : {
+        type : String
+    }
+
+},{timestamps : true})
+
+export const User = mongoose.model("User",userSchema)
